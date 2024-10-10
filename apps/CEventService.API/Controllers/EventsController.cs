84936ab6002c3cabb31c03f78fceb711766d8d94
JuanPabloArequipa <pablodavid.arequipa@fundacion-jala.org>
2024-10-10 14:43:58 -0400

    feat: EventsController.cs sample implemented with POST and GET

---
+++ apps/CEventService.API/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CEventService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetEvents()
+        {
+            var events = new[]
+            {
+                new { Id = 1, Name = "Music Festival", Date = "2024-10-15" },
+                new { Id = 2, Name = "Tech Conference", Date = "2024-11-05" },
+            };
+
+            return Ok(events);
+        }
+
+        
+        [HttpPost]
+        public IActionResult CreateEvent([FromBody] Event newEvent)
+        {
+            return CreatedAtAction(nameof(GetEvents), new { id = newEvent.Id }, newEvent);
+        }
+    }
+}
---
+++ apps/CEventService.API/Models/Event.cs
@@ -0,0 +1,8 @@
+﻿namespace CEventService.API.Controllers;
+
+public class Event
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Date { get; set; }
+}
