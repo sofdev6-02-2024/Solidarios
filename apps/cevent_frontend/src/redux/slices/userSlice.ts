5dd2baf7616b1fd4c59371070abfe23a925e798a
Fernando Mauricio Mamani Navarro <wmauchess@gmail.com>
2024-10-23 14:31:42 -0400

    fix: delete comments

--- apps/cevent_frontend/src/redux/slices/userSlice.ts
+++ apps/cevent_frontend/src/redux/slices/userSlice.ts
@@ -1,4 +1,3 @@
-// redux/slices/userSlice.ts
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface User {
--- apps/cevent_frontend/src/redux/store.ts
+++ apps/cevent_frontend/src/redux/store.ts
@@ -1,4 +1,3 @@
-// redux/store.ts
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import userReducer from './slices/userSlice';
