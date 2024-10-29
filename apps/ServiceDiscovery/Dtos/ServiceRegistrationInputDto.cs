ee5b5c32193571468288ff88275522336075dacc
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2024-10-29 14:27:07 -0400

    Models and dtos

---
+++ apps/ServiceDiscovery/Dtos/ServiceRegistrationInputDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceDiscovery.Dtos;
+
+public class ServiceRegistrationInputDto
+{
+    public required string ServiceName { get; set; }      
+    // ...../apigateway/serviceName/everything 
+    public required string Address { get; set; }           
+    public int Port { get; set; }                 
+    public required string HealthCheckEndpoint { get; set; }
+}
