bd9b3564b160f7957c716e0ac2230fd75aae89e7
gabitrujillo <gabrielatrujilloll153@gmail.com>
2024-10-29 23:39:40 -0400

    conflicts

--- apps/cevent_frontend/src/components/Footer.tsx
+++ apps/cevent_frontend/src/components/Footer.tsx
@@ -6,26 +6,50 @@ const Footer: React.FC = () => {
     <div className={styles.footer}>
       <div>
         <h1>Cvent</h1>
-        <h2>Descripción sobre qué es Cvent, Descripción sobre qué es Cvent, Descripción sobre qué es Cvent ,Descripción sobre qué es Cvent</h2>        
+        <h2>
+          Descripción sobre qué es Cvent, Descripción sobre qué es Cvent,
+          Descripción sobre qué es Cvent ,Descripción sobre qué es Cvent
+        </h2>
       </div>
       <div>
         <h1>Contacto</h1>
         <h2>
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" style={{ color: '#FFF', textDecoration: 'none' }}>Facebook</a>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: '#FFF', textDecoration: 'none' }}
+          >
+            Facebook
+          </a>
         </h2>
         <h2>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" style={{ color: '#FFF', textDecoration: 'none' }}>Instagram</a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: '#FFF', textDecoration: 'none' }}
+          >
+            Instagram
+          </a>
         </h2>
         <h2>
-          <a href="mailto:your-email@example.com" target="_blank" rel="noopener noreferrer" style={{ color: '#FFF', textDecoration: 'none' }}>Email</a>
+          <a
+            href="mailto:your-email@example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: '#FFF', textDecoration: 'none' }}
+          >
+            Email
+          </a>
         </h2>
       </div>
-      <div>      
+      <div>
         <h1>Información</h1>
         <h2>Subinfo</h2>
         <h2>Subinfo</h2>
         <h2>Subinfo</h2>
-      </div>      
+      </div>
     </div>
   );
 };
