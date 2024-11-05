11596a9d882cae450a3604a3fe348182cb0143af
gabitrujillo <gabrielatrujilloll153@gmail.com>
2024-11-05 15:11:46 -0400

    add footer the layout

--- apps/cevent_frontend/src/app/layout.tsx
+++ apps/cevent_frontend/src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
           <ReduxProvider>
             <ThemeRegistry>
               <Header />
+              <Footer />
               {children}
             </ThemeRegistry>
           </ReduxProvider>
