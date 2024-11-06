5cdc2571356a2b1320361151cd2bd281af638c90
victor-fundacion <victor.villca@fundacion-jala.org>
2024-11-06 18:16:01 -0400

    fix: deleted not in scope feautures

--- apps/cevent_frontend/src/app/create_event/page.tsx
+++ apps/cevent_frontend/src/app/create_event/page.tsx
@@ -1,36 +1,11 @@
 'use client';
 
-import { useSession } from 'next-auth/react';
-import { redirect, useRouter } from 'next/navigation';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 export default function CreateEvent() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (status === 'unauthenticated') {
-      router.push('/unauthorized');
-      router.refresh();
-    }
-  }, [session, status, router]);
-
-  if (status === 'loading') {
-    return (
-      <main>
-        <h1 className="text-4xl text-center">Create Event</h1>
-        <div className="text-center text-2xl">Loading...</div>
-      </main>
-    );
-  }
-
-  if (session) {
-    return (
-      <main>
-        <div>Create Event</div>
-      </main>
-    );
-  }
-
-  redirect('/unauthorized');
+  return (
+    <main>
+      <div>Create Event</div>
+    </main>
+  );
 }
--- apps/cevent_frontend/src/app/my_events/page.tsx
+++ apps/cevent_frontend/src/app/my_events/page.tsx
@@ -1,36 +1,10 @@
 'use client'
-import { redirect, useRouter } from 'next/navigation';
-import { routes } from '@/utils/navigation/Routes';
-import { Button } from "@mui/material";
-import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+
 
 export default function MyEventsPage() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-  
-  useEffect(() => {
-    if (status === 'unauthenticated') {
-      router.push('/unauthorized');
-      router.refresh();
-    }
-  }, [session, status, router]);
-
-  if (status === 'loading') {
-    return (
-      <main>
-        <h1 className="text-4xl text-center">Create Event</h1>
-        <div className="text-center text-2xl">Loading...</div>
-      </main>
-    );
-  }
-  if (session)
-  {
-    return (
-      <div>
-        <h1>My Events</h1>
-      </div>
-    )
-  }
-  redirect('/unauthorized')
+  return (
+    <div>
+      <h1>My Events</h1>
+    </div>
+  )
 }
