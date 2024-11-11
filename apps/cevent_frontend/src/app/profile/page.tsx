516d654e4bf539220f5547ce053d1bdbe6a4987c
karvlox <135563580+Karvlox@users.noreply.github.com>
2024-11-11 19:24:01 -0400

    feat: Solve Conflicts.

--- apps/cevent_frontend/src/app/profile/page.tsx
+++ apps/cevent_frontend/src/app/profile/page.tsx
@@ -1,25 +1,11 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import { signOut } from 'next-auth/react';
-import { Button } from '@mui/material';
 import AuthStatus from '@/components/AuthStatus';
 
 export default function ProfilePage() {
-  const router = useRouter();
-
-  const handleLogout = async () => {
-    await signOut({ redirect: false });
-    router.push('/');
-  };
-
+ 
   return (
     <div>
       <h1>Profile</h1>
       <AuthStatus />
-      <Button onClick={handleLogout} variant="contained" color="primary">
-        Log out
-      </Button>
     </div>
   );
 }
