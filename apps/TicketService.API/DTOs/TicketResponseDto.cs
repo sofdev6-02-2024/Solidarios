efdb98a33c150b674b299b2b6218ff3e673726d1
JuanPabloArequipa <pablodavid.arequipa@fundacion-jala.org>
2024-11-12 07:11:06 -0400

    feat: model and dtos structures setup

---
+++ apps/TicketService.API/DTOs/TicketResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TicketService.API.DTOs
+{
+    public class TicketResponseDto
+    {
+        public string TicketId { get; set; } = string.Empty;
+        public string QRContent { get; set; } = string.Empty;
+    }
+}
---
+++ apps/TicketService.API/Models/Ticket.cs
@@ -0,0 +1,13 @@
+﻿namespace TicketService.API.Models
+{
+    public class Ticket
+    {
+        public Guid TicketId { get; set; } = Guid.NewGuid();
+        public int EventId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string QRContent { get; set; } = string.Empty;
+        public bool IsUsed { get; set; } = false;
+        public DateTime DateIssued { get; set; } = DateTime.UtcNow;
+        public DateTime? DateUsed { get; set; }
+    }
+}
