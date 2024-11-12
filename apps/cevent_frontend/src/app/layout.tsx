ae0cf05f2441f0792d312e0cc4fe1ef9a5eaa528
Fernando Mauricio Mamani Navarro <wmauchess@gmail.com>
2024-11-12 09:24:11 -0400

    fix: footer

--- apps/cevent_frontend/src/app/layout.tsx
+++ apps/cevent_frontend/src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
               <Header />
 
               {children}
+              <Footer />
             </ThemeRegistry>
           </ReduxProvider>
         </body>
