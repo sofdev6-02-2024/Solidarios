4b0347c93f4850251cc25aadbee44091cc716b4f
JuanPabloArequipa <pablodavid.arequipa@fundacion-jala.org>
2024-11-12 14:27:52 -0400

    fix: delete comments

--- apps/TicketService.API/Program.cs
+++ apps/TicketService.API/Program.cs
@@ -6,8 +6,6 @@ using TicketService.API.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<QrCodeGeneratorUtility>();
@@ -30,7 +28,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
