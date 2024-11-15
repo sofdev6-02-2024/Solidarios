85cc080c69fc4187a3ce12c6f80b0ae0548855fd
victor-fundacion <victor.villca@fundacion-jala.org>
2024-11-15 13:15:48 -0400

    feat: email template html and dotenv use

--- apps/NotificationService.API/Repositories/EmailRepository.cs
+++ apps/NotificationService.API/Repositories/EmailRepository.cs
@@ -1,24 +1,44 @@
+using System.IO;
+using dotenv.net;
+using dotenv.net.Utilities;
 using MailKit.Net.Smtp;
 using MailKit.Security;
 using MimeKit;
+using MimeKit.Text;
 
-namespace NotificationService.Repositories;
-public class EmailRepository : IEmailRepository
+namespace NotificationService.Repositories
 {
-    public void SendEmail(string email, string subject, string body)
+    public class EmailRepository : IEmailRepository
     {
-        var message = new MimeMessage();
-        message.From.Add(new MailboxAddress("Cevent App", "solidariossolidarios62@gmail.com"));
-        message.To.Add(new MailboxAddress("", email));
-        message.Subject = subject;
-        message.Body = new TextPart("plain") { Text = body };
-
-        using (var client = new SmtpClient())
+        private readonly string _emailTemplate;
+        public EmailRepository()
         {
-            client.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
-            client.Authenticate("solidariossolidarios62@gmail.com", "");
-            client.Send(message);
-            client.Disconnect(true);
+            DotEnv.Load();
+            _emailTemplate = File.ReadAllText("Templates/EmailTemplate.html");
+        }
+
+        public void SendEmail(string email, string subject, string body)
+        {
+            var appMailPassword = EnvReader.GetStringValue("APP_MAIL_PASSWORD");
+            var appMailEmail = EnvReader.GetStringValue("APP_MAIL_EMAIL");
+            var appMailName = EnvReader.GetStringValue("APP_MAIL_NAME");
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(appMailName, appMailEmail));
+            message.To.Add(new MailboxAddress("", email));
+            message.Subject = subject;
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = _emailTemplate.Replace("{{MESSAGE_BODY}}", body);
+            message.Body = bodyBuilder.ToMessageBody();
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                client.Authenticate(appMailEmail, appMailPassword);
+                client.Send(message);
+                client.Disconnect(true);
+            }
         }
     }
 }
