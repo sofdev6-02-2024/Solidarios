7348bbf36d459c43495a355dd545222eb5ec652d
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2024-11-21 19:58:41 -0400

    feat: created at added

--- apps/CEventService.API/Models/User.cs
+++ apps/CEventService.API/Models/User.cs
@@ -6,7 +6,6 @@ public class User : BaseEntity<Guid>
     public required string LastName { get; set; }
     public required string Email { get; set; }
     public required string PhoneNumber { get; set; }
-    public DateTime CreatedAt { get; set; }
 
     public ICollection<Event> EventsCreated { get; set; }
     public ICollection<Registration> Registrations { get; set; }
