ab6db6bcf44ad1515bd55bbe56d57a69597093b6
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2024-11-21 20:41:08 -0400

    feat: user photo url

--- apps/CEventService.API/DTOs/User/UserInputDto.cs
+++ apps/CEventService.API/DTOs/User/UserInputDto.cs
@@ -7,4 +7,5 @@ public class UserInputDto : IMapFrom<Models.User>
     public required string LastName { get; set; }
     public required string Email { get; set; }
     public required string PhoneNumber { get; set; }
+    public string? PhotoUrl { get; set; } 
 }
