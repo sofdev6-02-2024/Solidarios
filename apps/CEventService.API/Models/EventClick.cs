59be0976e4ff762dcc2a7c2e19e0778307fea5ce
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2024-11-25 12:21:29 -0400

    fix: model updated

--- apps/CEventService.API/Models/EventClick.cs
+++ apps/CEventService.API/Models/EventClick.cs
@@ -3,8 +3,8 @@
 public class EventClick : BaseEntity<int>
 {
     public int EventId { get; set; }
-    public Guid UserId { get; set; }
+    public Guid? UserId { get; set; }
 
     public Event Event { get; set; }
-    public User User { get; set; }
+    public User? User { get; set; }
 }
