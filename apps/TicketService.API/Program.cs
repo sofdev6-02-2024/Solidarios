a585bd9107f31085cdc0a84f7c4c8645c9c8ae9a
JuanPabloArequipa <pablodavid.arequipa@fundacion-jala.org>
2024-11-26 00:36:19 -0400

    fix: credentials moved to .env

--- apps/TicketService.API/Program.cs
+++ apps/TicketService.API/Program.cs
@@ -1,10 +1,20 @@
+using dotenv.net;
+using dotenv.net.Utilities;
 using Microsoft.EntityFrameworkCore;
 using TicketService.API.Database;
 using TicketService.API.Repositories;
 using TicketService.API.Services;
 using TicketService.API.Utilities;
 
+DotEnv.Load(new DotEnvOptions(
+    envFilePaths: new[] { ".env" },
+    ignoreExceptions: false,
+    probeForEnv: true,
+    probeLevelsToSearch: 6
+));
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = EnvReader.GetStringValue("CONNECTION_STRING");
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,8 +23,7 @@ builder.Services.AddScoped<ITicketService, TicketService.API.Services.TicketServ
 builder.Services.AddScoped<ITicketRepository, TicketRepository>();
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TicketDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<TicketDbContext>(options => { options.UseSqlServer(connectionString); });
 
 builder.Services.AddCors(options =>
 {
