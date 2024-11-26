2ffd451fc7b32f487a20861d7e0de0c053dfd2ad
karvlox <135563580+Karvlox@users.noreply.github.com>
2024-11-26 12:06:58 -0400

    feat: Added format prettier

--- apps/cevent_frontend/src/app/api/tickets/list/route.ts
+++ apps/cevent_frontend/src/app/api/tickets/list/route.ts
@@ -9,19 +9,16 @@ const BASE_URL = process.env.NEXT_PUBLIC_API_URL || '';
  * @returns list of all tickets or an error message
  */
 export async function GET(request: Request) {
-    const fullUrl = `${BASE_URL}/ticket-service/api/Ticket`;
-  
-    try {
-      const response = await axios.get(fullUrl);
-      return NextResponse.json(response.data, { status: 200 });
-    } catch (error: any) {      
-      const statusCode = error.response?.status || 500;
-      const errorMessage =
-        error.response?.data?.message || 'Failed to fetch tickets';
-  
-      return NextResponse.json(
-        { error: errorMessage },
-        { status: statusCode }
-      );
-    }
+  const fullUrl = `${BASE_URL}/ticket-service/api/Ticket`;
+
+  try {
+    const response = await axios.get(fullUrl);
+    return NextResponse.json(response.data, { status: 200 });
+  } catch (error: any) {
+    const statusCode = error.response?.status || 500;
+    const errorMessage =
+      error.response?.data?.message || 'Failed to fetch tickets';
+
+    return NextResponse.json({ error: errorMessage }, { status: statusCode });
   }
+}
