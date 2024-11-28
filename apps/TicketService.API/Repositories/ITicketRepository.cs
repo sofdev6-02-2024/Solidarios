b58401eada02cb8164432d92b5404e23abe3c301
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2024-11-28 12:28:17 -0400

    feat: ticket request by user id

--- apps/TicketService.API/Repositories/ITicketRepository.cs
+++ apps/TicketService.API/Repositories/ITicketRepository.cs
@@ -1,10 +1,13 @@
-﻿namespace TicketService.API.Repositories
+﻿using System.Collections;
+
+namespace TicketService.API.Repositories
 {
     using TicketService.API.Models;
     
     public interface ITicketRepository
     {
         Task<Ticket> CreateTicketAsync(Ticket ticket);
+        Task<ICollection<Ticket>> CreateTicketsAsync(ICollection<Ticket> tickets);
         Task<IEnumerable<Ticket>> GetAllTicketAsync();
         Task<Ticket?> GetTicketByIdAsync(Guid ticketId);
         Task<Ticket?> GetTicketByQrContentAsync(string qrContent);
--- apps/TicketService.API/Repositories/TicketRepository.cs
+++ apps/TicketService.API/Repositories/TicketRepository.cs
@@ -20,6 +20,13 @@ namespace TicketService.API.Repositories
             return ticket;
         }
 
+        public async Task<ICollection<Ticket>> CreateTicketsAsync(ICollection<Ticket> tickets)
+        {
+            _context.Tickets.AddRange(tickets);
+            await _context.SaveChangesAsync();
+            return tickets;
+        }
+
         public async Task<IEnumerable<Ticket>> GetAllTicketAsync()
         {
             return await _context.Tickets.ToListAsync();
